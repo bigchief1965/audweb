2f7c6cf954546909db9810211a2d012cd52ecc61
bigchief1965 <97263917+bigchief1965@users.noreply.github.com>
2023-07-02 02:17:03 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,117 +1,83 @@
-var pink;
-var white;
-var brown;
-var track1;
-var track2;
-var track3;
-let incr = 1;
-const laodin = document.querySelector("#lodin")
+const loadText = document.querySelector(".loading-text");
 
-// thank you developer.mozilla.org
+//thank you mdn web docs developer.mozilla.org
 //console.clear();
+
 let audioCtx = null;
+
+// Provide a start button so demo can load tracks from an event handler for cross-browser compatibility
 const loadButton = document.querySelector("#loadbutton");
+console.log(loadButton);
+
+// Select all list elements
 const trackEls = document.querySelectorAll("li");
 console.log(trackEls);
+
+// Loading function for fetching the audio file and decode the data
 async function getFile(filepath) {
   const response = await fetch(filepath);
   const arrayBuffer = await response.arrayBuffer();
   const audioBuffer = await audioCtx.decodeAudioData(arrayBuffer);
   return audioBuffer;
 }
+
+// Function to call each file and return an array of decoded files
 async function loadFile(filePath) {
   const track = await getFile(filePath);
   return track;
 }
+
 let offset = 0;
+// Create a buffer, plop in data, connect and play -> modify graph here if required
+//play track
 function playTrack(audioBuffer) {
   const trackSource = new AudioBufferSourceNode(audioCtx, {
     buffer: audioBuffer,
   });
   trackSource.connect(audioCtx.destination);
+  //eek
   trackSource.loop = true;
+
   if (offset == 0) {
     trackSource.start();
     offset = audioCtx.currentTime;
   } else {
     trackSource.start(0, audioCtx.currentTime - offset);
   }
+
   return trackSource;
 }
-//LOAD ALL THE FILES INTO ME VARIABLES
+
+//load the tracks
 loadButton.addEventListener("click", () => {
   if (audioCtx != null) {
     return;
   }
+
   audioCtx = new AudioContext();
+
+  document.querySelector("#loadbutton").hidden = true;
+
   trackEls.forEach((el, i) => {
+    // Get children
     const anchor = el.querySelector("a");
+    //const loadText = el.querySelector("p");
+    const playButton = el.querySelector(".playbutton");
+
+    // Load file
     loadFile(anchor.href).then((track) => {
-      if(incr === 1) {
-        track1 = track;
-      } else if(incr === 2) {
-        track2 = track;
-      } else if(incr === 3) {
-        track3 = track;
-      } else {
-        console.log("track load error");
-      }
       el.dataset.loading = "false";
+
+      // Allow play on click
+      playButton.addEventListener("click", () => {
+        // check if context is in suspended state (autoplay policy)
+        if (audioCtx.state === "suspended") {
+          audioCtx.resume();
+        }
+
+        playTrack(track);
+        playButton.dataset.playing = true;
+      });
     });
   });
-  pink = track1;
-  white = track2;
-  brown = track3;
-  laodin.innerHTML = "loadided";
 });
-
-//eek pnb999.2
-const audioText = document.getElementById("audioText");
-let currentAudio = pink;
-let currentAudioP;
-let playing = false;
-let wasPlaying = false;
-
-function playPause() {
-  if(playing === false) {
-    if (audioCtx.state === "suspended") {
-      audioCtx.resume();
-    }
-    currentAudioP = playTrack(currentAudio);
-    playing = true;
-  } else {
-    currentAudioP.stop();
-    playing = false;
-  }
-}
-function pauseEverything() {
-  try {
-    currentAudioP.stop();
-  } catch (error) {
-    console.log("dobetter");
-  }
-}
-function amButton(audio) {
-  if(currentAudio === audio) {
-      wasPlaying = true;
-  } else {
-      wasPlaying = false;
-      currentAudio = audio;
-  }
-  switch(currentAudio) {
-    case pink:
-        audioText.innerHTML = "pink noise";
-        break;
-    case white:
-        audioText.innerHTML = "white noise";
-        break;
-    case brown:
-        audioText.innerHTML = "brown noise";
-        break;
-  }
-  //pauseEverything();
-  playPause();
-  if(playing === false && wasPlaying === false) {
-      playPause();
-  }
-}
